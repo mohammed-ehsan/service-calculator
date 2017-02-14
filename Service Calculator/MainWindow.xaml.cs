543ce163e46dad74cf6ee7936793f16d9607f3dd
mohammed_ehsan <developer1engineer@gmail.com>
2017-02-14 22:32:31 +0300

    initial commit first working version with basic functionality

--- Service Calculator/MainWindow.xaml.cs
+++ Service Calculator/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Globalization;
 
 namespace Service_Calculator
 {
@@ -21,7 +22,44 @@ namespace Service_Calculator
     {
         public MainWindow()
         {
+            
             InitializeComponent();
+            CurrentDataPicker.SelectedDate = DateTime.Now;
+            txtbx_result.Text = "الرجاء تحديد تاريخ التعيين !";
         }
+
+        private void EmploymentDataPicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DisplayServiceDuration();
+        }
+
+        private void CurrentDataPicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DisplayServiceDuration();
+        }
+
+        private void DisplayServiceDuration()
+        {
+            if (EmploymentDataPicker.SelectedDate.HasValue && CurrentDataPicker.SelectedDate.HasValue)
+            {
+                txtbx_result.Text ="مدة الخدمة = " + CalculateService(CalculateDays((DateTime)EmploymentDataPicker.SelectedDate, (DateTime)CurrentDataPicker.SelectedDate)).ToString();
+            }
+        }
+
+        private TimeSpan CalculateDays(DateTime date1, DateTime date2)
+        {
+            TimeSpan time = date2 - date1;
+            return time;
+        }
+        private ServiceDuration CalculateService(TimeSpan span)
+        {
+            ServiceDuration duration = new Service_Calculator.ServiceDuration();
+            duration.Years = (int)(span.Days / 365.25);
+            duration.Months = (int)((span.Days - duration.Years * 365.25) / 30);
+            duration.Days = (int)(span.Days - duration.Years * 365.25 - duration.Months*30);
+            return duration;
+        }
+        
+        
     }
 }
---
+++ Service Calculator/ServiceDuration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Service_Calculator
+{
+    public struct ServiceDuration
+    {
+        public int Days;
+        public int Months;
+        public int Years;
+        public override string ToString()
+        {
+            return Years + " سنة ، " + Months + " شهر ، " + Days + " يوم.";
+        }
+
+    }
+}
